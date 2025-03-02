4b1556ef475ca379934b1e330b1aa96b69c6ba72
dywkat <katya.kolosovaaa@gmail.com>
2025-03-02 22:30:36 +0300

    Completed task

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Kolosova Ekaterina");
+
+        Scanner scan = new Scanner(System.in);
+        int icome = scan.nextInt();
+        int spending = scan.nextInt();
+        System.out.println("Итого (руб):");
+        System.out.println(icome - spending);
+    }
+}
