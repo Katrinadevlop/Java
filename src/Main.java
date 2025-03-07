546ffce670c4f9d55e01e9bb1f4732202867480c
dywkat <katya.kolosovaaa@gmail.com>
2025-03-07 17:42:53 +0300

    Homework 3

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+//Условные операторы и циклы
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int count = 0;
+        while(true) {
+            System.out.println("Введите год: ");
+            int yearLeap = scanner.nextInt();
+            System.out.println("Введите количество дней: ");
+            int yearDays = scanner.nextInt();
+
+            int res = isLeapYear(yearLeap);
+
+            if (res == yearDays)
+                count++;
+            else {
+                System.out.println("Неправильно! В этом году " + res + " дней!");
+                System.out.println("Набрано очков: " + count);
+                break;
+            }
+        }
+    }
+
+    private static int isLeapYear(int yearLeap) {
+        int res = (yearLeap % 4 == 0 || (yearLeap / 4 == 0 && yearLeap / 100 != 0)) ? 366 : 365;
+        return res;
+    }
+}
